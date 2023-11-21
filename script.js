e8828f78305da9163facbf9f4fd0d47946edf04a
Muhamad Daffa <muhamaddaffa113@gmail.com>
2023-11-21 19:28:05 +0700

    updating responsive navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+function resNav() {
+    var x = document.getElementById("resNavbar");
+
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
